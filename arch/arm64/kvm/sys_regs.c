0cb4e7c6de04294c04e4ed67ec71a07d710fdc00
tonyst2 <mishachev21@gmail.com>
2025-11-06 23:02:01 +0300

    Revert "KVM: arm64: sys_regs: fix Clang -Wuninitialized-const-pointer warning"
    This reverts commit b684f42f1b8e0e1ad6452c4c12e0ebaa010a4f11.

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -1262,9 +1262,7 @@ static bool access_clidr(struct kvm_vcpu *vcpu, struct sys_reg_params *p,
 	if (p->is_write)
 		return write_to_read_only(vcpu, p, r);
 
-	u64 clidr = 0;                     /* <---- добавить инициализацию */
-	get_clidr_el1(NULL, &clidr);
-	p->regval = clidr;
+	p->regval = read_sysreg(clidr_el1);
 	return true;
 }
 
